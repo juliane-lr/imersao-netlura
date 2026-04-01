dee4275936a303c0138e95642468f5fee04ca364
Juliane Lopes da Rocha <ju.lopesrocha@gmail.com>
2026-04-01 15:09:07 -0300

    Add files via upload

---
+++ catalogo/js/data.js
@@ -0,0 +1,41 @@
+export const categories = [
+    {
+        title: "Épicos",
+        items: [
+            {
+                img: "https://upload.wikimedia.org/wikipedia/en/6/67/Forrest_Gump_poster.jpg", top10: true, badge: "Clássico", badgeColor: "red", progress: 0,
+                youtube: "https://www.youtube.com/watch?v=bLvqoHBptjg"
+            },
+            {
+                img: "https://aventurasnahistoria.com.br/wp-content/uploads/entretenimento/gladiador_2_VvnGVes.jpg", progress: 0,
+                youtube: "https://www.youtube.com/watch?v=cXg62-t8BWs"
+            },
+            {
+                img: "https://i.ytimg.com/vi/OQgySPQ5M3Y/maxresdefault.jpg", progress: 0,
+                youtube: "https://www.youtube.com/watch?v=zckJCxYxn1g"
+            },
+            {
+                img: "https://ingresso-a.akamaihd.net/img/cinema/cartaz/14413-destaque.jpg", progress: 0,
+                youtube: "https://www.youtube.com/watch?v=a06zxOyQrAs"
+            },
+        ]
+    },
+    {
+        title: "Séries",
+        items: [
+            { img: "https://resizing.flixster.com/KHP8WIWqGr-3MmT1Sa9GvDtb3Q8=/fit-in/705x460/v2/https://resizing.flixster.com/-XZAfHZM39UwaGJIFWKAE8fS0ak=/v3/t/assets/p185008_b_h9_ac.jpg", top10: true, badge: "Nova temporada", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=tNcDHWpselE" },
+            { img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQuX_01R6QUqqhYYE6nelfGBEdNttqR45381g&s", top10: true, youtube: "https://www.youtube.com/watch?v=rCj-Fb1OmXg" },
+            { img: "https://m.media-amazon.com/images/S/pv-target-images/b58657c57cd6b28bd8d9686042842c9d3285e5961284c6238825360b6f2d69ef.png", badge: "Novo episódio", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=bBervTlBurY" },
+            { img: "https://http2.mlstatic.com/D_NQ_NP_931287-MLB94038820487_102025-O.webp", badge: "Novidade", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=iySNbu_Gr98" },
+        ]
+    },
+    {
+        title: "Para maratonar",
+        items: [
+            { img: "https://images.unsplash.com/photo-1574375927938-d5a98e8ffe85?q=80&w=600&auto=format&fit=crop", top10: true, youtube: "https://www.youtube.com/watch?v=wLo9bfgla4k" },
+            { img: "https://images.unsplash.com/photo-1574375927938-d5a98e8ffe85?q=80&w=600&auto=format&fit=crop", top10: true, badge: "Novidade", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=wLo9bfgla4k" },
+            { img: "https://images.unsplash.com/photo-1574375927938-d5a98e8ffe85?q=80&w=600&auto=format&fit=crop", top10: true, badge: "Novo episódio", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=wLo9bfgla4k" },
+            { img: "https://images.unsplash.com/photo-1574375927938-d5a98e8ffe85?q=80&w=600&auto=format&fit=crop", top10: true, badge: "Novo episódio", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=wLo9bfgla4k" },
+        ]
+    }
+];
