b7c180236067d62707f2037e9b60592280883530
Juliane Lopes da Rocha <ju.lopesrocha@gmail.com>
2026-04-01 21:25:02 -0300

    Add files via upload

---
+++ catalogo/js/data.js
@@ -0,0 +1,45 @@
+export const categories = [
+    {
+        title: "Épicos",
+        items: [
+            {
+                img: "https://upload.wikimedia.org/wikipedia/en/6/67/Forrest_Gump_poster.jpg", top10: true, badge: "Clássico", badgeColor: "red", progress: 0,
+                youtube: "https://www.youtube.com/watch?v=bLvqoHBptjg"
+            },
+            {
+                img: "https://aventurasnahistoria.com.br/wp-content/uploads/entretenimento/gladiador_2_VvnGVes.jpg", progress: 0,
+                youtube: "https://www.youtube.com/watch?v=cXg62-t8BWs"
+            },
+            {
+                img: "https://i.ytimg.com/vi/OQgySPQ5M3Y/maxresdefault.jpg", progress: 0,
+                youtube: "https://www.youtube.com/watch?v=zckJCxYxn1g"
+            },
+            {
+                img: "https://ingresso-a.akamaihd.net/img/cinema/cartaz/14413-destaque.jpg", progress: 0,
+                youtube: "https://www.youtube.com/watch?v=a06zxOyQrAs"
+            },
+        ]
+    },
+    {
+        title: "Séries",
+        items: [
+            { img: "https://resizing.flixster.com/KHP8WIWqGr-3MmT1Sa9GvDtb3Q8=/fit-in/705x460/v2/https://resizing.flixster.com/-XZAfHZM39UwaGJIFWKAE8fS0ak=/v3/t/assets/p185008_b_h9_ac.jpg", top10: true, badge: "Nova temporada", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=tNcDHWpselE" },
+            { img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQuX_01R6QUqqhYYE6nelfGBEdNttqR45381g&s", top10: true, youtube: "https://www.youtube.com/watch?v=rCj-Fb1OmXg" },
+            { img: "https://m.media-amazon.com/images/S/pv-target-images/b58657c57cd6b28bd8d9686042842c9d3285e5961284c6238825360b6f2d69ef.png", badge: "Novo episódio", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=bBervTlBurY" },
+            { img: "https://br.web.img3.acsta.net/r_654_368/newsv7/20/09/01/19/45/5822933.jpg", badge: "Novidade", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=Nu5z3AT2jv8" },
+        ]
+    },
+    {
+        title: "Mais assistidos",
+        items: [
+            { img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR3QCk2DrPJRXpNq0nvRTAsFTowJ_mMxmtvHQ&s", top10: true, 
+                youtube: "https://www.youtube.com/watch?v=ZoknZInQM3o" },
+            { img: "https://cinecambui.com.br/storage/movies/lpPdM5qjkRHBj4nqWZqx2sT9e8tSZfbzQO7og06w.webp", top10: true, badge: "Novidade", badgeColor: "red", 
+                youtube: "https://www.youtube.com/watch?v=4SP2X7KCulU" },
+            { img: "https://i.ytimg.com/vi/AHMdF9KTBnY/hq720.jpg", top10: true, badge: "Novo episódio", badgeColor: "red", 
+                youtube: "https://www.youtube.com/watch?v=AOBPXs_euPA" },
+            { img: "https://br.web.img2.acsta.net/medias/nmedia/18/90/43/36/20096333.jpg", top10: true, badge: "Novo episódio", badgeColor: "red", 
+                youtube: "https://www.youtube.com/watch?v=w1O5LThfp7w" },
+        ]
+    }
+];
